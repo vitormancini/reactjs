ee3add420166d41a24e7033ae44e065a92026cd6
Vitor Mancini <vitor.mancini@dell.com>
2025-10-17 13:36:22 -0300

    Adicionando responsividade ao layout da calculadora

--- fundamentos/src/App.tsx
+++ fundamentos/src/App.tsx
@@ -4,7 +4,7 @@ import { Calculator } from "./Calculator";
 function App() {
   return (
     <div style={{ color: "var(--text)", fontFamily: "var(--font-sans)" }}>
-      <main className="py-28 px-4">
+      <main className="py-28 px-4 flex flex-col sm:flex-row items-center sm:items-stretch gap-2">
         <Calculator operation="1 + 1" result={2} />
       </main>
     </div>
